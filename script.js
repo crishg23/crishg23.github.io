da8b2f72bf9ce067bf9588fb32f86bf1eabaa354
crishg23 <crishg23@gmail.com>
2023-04-06 13:39:39 +0100

    commit

--- script.js
+++ script.js
@@ -1,32 +1,31 @@
-	let lyrics = '';
- 
-	for(let i=99;i>=0;i--){
-		document.getElementById('lyrics').innerHTML=lyrics;
-		
-		if(i>2){
-			lyrics+=i+ " bottles of beer on the wall, ";
-			lyrics+=i+" bottles of beer"+ "<br>";
-			lyrics+="Take one down and pass it around, ";
-			lyrics+=(i-1)+" bottles of beer on the wall"+ "<br>"+ "<br>";
-		}
-		
-		else if(i==2){
-			lyrics+=i+ " bottles of beer on the wall, ";
-			lyrics+=i+" bottles of beer"+ "<br>";
-			lyrics+="Take one down and pass it around, ";
-			lyrics+=(i-1)+" bottle of beer on the wall"+ "<br>"+ "<br>";
-		}
-		
-		else if(i==1){
-			lyrics+=i+ " bottle of beer on the wall, ";
-			lyrics+=i+" bottle of beer"+ "<br>";
-			lyrics+="Take one down and pass it around, ";
-			lyrics+=(i-1)+" bottles of beer on the wall!"+ "<br>"+ "<br>";
-			lyrics+="No more bottles of beer on the wall, no more bottles of beer."+"<br>"+"Go to the store and buy some more, 99 bottles of beer on the wall"+ "<br>";	
-		}	
 
-			
-	}
-	
+let groceries=[];
 
-	
+function addItem() {
+    const itemInput = document.getElementById("item");
+    const item = itemInput.value.trim();
+    if (item !== '') {
+        groceries.push(item);
+        itemInput.value = '';
+        displayList();
+    }
+}
+
+function removeItem(index) {
+    groceries.splice(index, 1);
+    displayList();
+}
+
+function displayList() {
+    const list = document.getElementById("list");
+    list.innerHTML = '';
+    groceries.forEach((item, index) => {
+        const li = document.createElement("li");
+        const removeBtn = document.createElement("button");
+        removeBtn.innerText = "Remove";
+        removeBtn.onclick = () => removeItem(index);
+        li.innerText = item;
+        li.appendChild(removeBtn);
+        list.appendChild(li);
+    });
+}
